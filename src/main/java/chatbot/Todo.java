5f4c92fc5a2604a273a81047c4981d6b58b8c1f4
Bryan Yeo <bryaniyeo3125@gmail.com>
2026-03-08 17:50:16 +0800

    A-JavaDoc: add JavaDoc comments to classes and methods

--- src/main/java/chatbot/Todo.java
+++ src/main/java/chatbot/Todo.java
@@ -1,11 +1,24 @@
 package chatbot;
 
+/**
+ * Represents a todo task without any date or time information.
+ */
 class Todo extends Task {
 
+    /**
+     * Constructs a todo task with a description.
+     *
+     * @param description Description of the todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns the type icon for a todo task.
+     *
+     * @return "T"
+     */
     @Override
     protected String getTypeIcon() {
         return "T";
